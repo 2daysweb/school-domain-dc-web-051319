e57606766592485dc0f6cacad617c1434d34e1ff
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-18 19:49:23 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -23,13 +23,10 @@ class School
     
     def sort_by_key
       @roster.keys.sort.to_h
-    end 
   end 
     
     
     def sort
-      arr_values = sort_by_value
-      sorted_key_hash = @roster.sort.to_h
       sorted_key_hash.each do |k,v|
           arr_values.each do |arr| 
           if @roster[k] == arr
