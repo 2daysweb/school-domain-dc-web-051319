6b4482e4e8f9b3499dad3dba72a84305d04788db
balanced-anchor-3429 <sahnunhm@gmail.com>
2019-05-18 19:46:12 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,54 @@
-# code here!
+require 'pry'
+class School
+  
+  attr_accessor :roster
+    
+    def initialize(roster)
+      @roster = {}
+    end 
+
+
+    def add_student(name, grade)
+      if roster[grade] == nil 
+         @roster[grade] = [] 
+         @roster[grade].push(name)
+      else 
+         @roster[grade].push(name)
+    end 
+  end
+  
+    def grade(level)
+      @roster[level]
+    end 
+    
+    def sort_by_value
+      sorted_vals = @roster.values 
+      #binding.pry
+      sorted_vals.collect do |arr|
+        arr.sort 
+      #binding.pry
+    end 
+  end 
+    
+    
+    def sort
+      arr_values = sort_by_value
+      sorted_key_hash = @roster.sort.to_h
+      sorted_key_hash.each do |k,v|
+          arr_values.each do |arr| 
+          if @roster[k] == arr
+          binding.pry
+          @roster[k] = arr
+        else
+          @roster[k] = @roster[k]
+          binding.pry
+        end
+      end
+    end
+  end
+end
+          
+      
+  
+    
+    
